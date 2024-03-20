8c620c3641befb6a506af4168c9abeed3e2b7929
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-20 13:27:44 +0100

    login request

--- src/dtos/requests/RegisterRequest.java
+++ src/dtos/requests/RegisterRequest.java
@@ -1,13 +1,26 @@
 package dtos.requests;
 
 public class RegisterRequest {
+    private String username;
+    private String password;
+
+    public RegisterRequest() {
+
+    }
+
     public RegisterRequest(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    private String username;
-    private String password;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
